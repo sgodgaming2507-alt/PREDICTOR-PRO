adab2b35153455ebbb42af91bec033578baaeacc
sgodgaming2507-alt <sgodgaming2507@gmail.com>
2026-09-20 04:39:51 +0530

    Create ProtectedSketchApplication.java

---
+++ app/src/main/java/com/yadav/a1/ProtectedSketchApplication.java
@@ -0,0 +1,10 @@
+package com.yadav.a1;
+
+import android.app.Application;
+
+public class ProtectedSketchApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
