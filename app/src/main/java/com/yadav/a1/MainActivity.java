8ccdcbc25834d20cf1bcf48f57dc68f0a13fb3e5
sgodgaming2507-alt <sgodgaming2507@gmail.com>
2026-09-23 01:21:08 +0530

    Update MainActivity.java

--- app/src/main/java/com/yadav/a1/MainActivity.java
+++ app/src/main/java/com/yadav/a1/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends Activity {
 
         // System overlay permission check for Android 6.0+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
-            Intent intent = new Intent(Intent.ACTION_MANAGE_OVERLAY_PERMISSION,
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                     Uri.parse("package:" + getPackageName()));
             startActivityForResult(intent, 1234);
             return;
